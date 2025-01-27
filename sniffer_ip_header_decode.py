91c0c25a104c7d895f1c897376084e119cea03a9
1amplant <raphaeltzy13@gmail.com>
2025-01-28 00:06:24 +0800

    Integrated ICMP class to parse ICMP packets and updated sniff function to detect, process, and display ICMP packet details including type and code.

--- sniffer_ip_header_decode.py
+++ sniffer_ip_header_decode.py
@@ -25,15 +25,27 @@ class IP:
 
         # map protocol constants to their names
         self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
+        
         try:
             self.protocol = self.protocol_map[self.protocol_num]
+        
         except Exception as e:
             print('%s No protocol for %s' % (e, self.protocol_num))
             self.protocol = str(self.protocol_num)
 
+class ICMP:
+    def __init__(self, buff):
+        header = struct.unpack('<BBHHH', buff)
+        self.type = header[0]
+        self.code = header[1]
+        self.sum = header[2]
+        self.id = header[3]
+        self.seq = header[4]
+
 def sniff(host):
     if os.name == 'nt':
         socket_protocol = socket.IPPROTO_IP
+    
     else:
         socket_protocol = socket.IPPROTO_ICMP
 
@@ -52,9 +64,25 @@ def sniff(host):
             # create an IP header from the first 20 bytes
             ip_header = IP(raw_buffer[0:20])
 
-            # print the detected protocol and hosts
-            print('Protocol: %s %s -> %s' %
-                  (ip_header.protocol, ip_header.src_address, ip_header.dst_address))
+            # if it's ICMP, we want it
+            if ip_header.protocol == "ICMP":
+                print('Protocol: %s %s -> %s' %
+                      (ip_header.protocol,
+                       ip_header.src_address,
+                       ip_header.dst_address))
+                
+                print(f'Version: {ip_header.ver}')
+                print(f'Header Length: {ip_header.ihl} TTL: {ip_header.ttl}')
+                
+                # calculate where our ICMP packet starts
+                offset = ip_header.ihl * 4
+                buf = raw_buffer[offset:offset + 8]
+                
+                # create our ICMP structure
+                icmp_header = ICMP(buf)
+                print('ICMP -> Type: %s Code: %s\n' %
+                      (icmp_header.type, icmp_header.code))
+
     except KeyboardInterrupt:
         # if we're on Windows, turn off promiscuous mode
         if os.name == 'nt':
@@ -64,6 +92,7 @@ def sniff(host):
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         host = sys.argv[1]
+    
     else:
         host = '192.168.1.203'
     sniff(host)
